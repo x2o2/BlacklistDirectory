29d5f439241a0d26dbc4a0b6a1132a2af24d0a1d
None <None>
2024-05-12 15:33:58 +0800

    ('总计：287', {'只能赞扬不能批评的': 19, '名': 37, '地区': 32, '热点事件': 7, '生活常识': 132, '诈骗的': 60})

--- 统计.py
+++ 统计.py
@@ -43,6 +43,27 @@ def 黑名单数量(*file_paths, directory='黑名单', endswith=".md"):
             word_counts[filename] = f"读取文件时出错: {e}"
     return f"总计：{sum(word_counts.values())}",word_counts
 
+def 输出黑名单txt(*file_paths, directory='黑名单', endswith=".md"):
+    """
+    计算多个文件中的单词数量，处理多种类型的空白字符。
+
+    参数:
+    *file_paths: 一个包含多个文件路径的元组。
+
+    """
+    words = []
+    for file_path in file_paths[0]:
+        try:
+            with open(os.path.join(directory, file_path), 'r', encoding='utf-8') as file:
+                content = file.read()
+                # 使用默认的split()来正确处理所有类型的空白字符
+                words += content.split()
+        except Exception as e:
+            print(f"读取文件时出错: {e}")
+
+    with open("黑名单.txt", 'w', encoding='utf-8') as file:
+        file.write(" ".join(words))
 
 黑名单文件 = 统计黑名单文件()
 print(黑名单数量(黑名单文件))
+输出黑名单txt(黑名单文件)
